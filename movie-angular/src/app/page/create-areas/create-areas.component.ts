43d2b154c81dd4d06112ee2de8d430b655c9d13d
yuelinghunyu <1561624238@qq.com>
2018-05-13 23:28:31 +0800

    create-areas add panel layout

--- movie-angular/src/app/page/create-areas/create-areas.component.ts
+++ movie-angular/src/app/page/create-areas/create-areas.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,state,style,animate,transition} from '@angular/animations';
 
 @Component({
   selector: 'app-create-areas',
   templateUrl: './create-areas.component.html',
-  styleUrls: ['./create-areas.component.css']
+  styleUrls: ['./create-areas.component.css'],
+  animations:[
+    trigger('panel-left',[
+      state('inactive', style({
+        left:'-400px'
+      })),
+      state('active',   style({
+        left:'0'
+      })),
+      transition('inactive => active', animate('200ms ease-in')),
+      transition('active => inactive', animate('200ms ease-out'))
+    ]),
+  ]
 })
 export class CreateAreasComponent implements OnInit {
-
+  private state:string;
   constructor() { }
 
   ngOnInit() {
+    this.state = 'inactive';
   }
-
+  addPanel(){
+    this.state = this.state === 'active' ? 'inactive' : 'active';
+  }
 }
